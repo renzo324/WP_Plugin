fa31ab22364f7903fdff98ecc096cf375c27b942
Lorenzo Ibay <finnthehuman324@gmail.com>
2020-03-03 23:36:57 +0800

    start refactor

--- sud_payments.php
+++ sud_payments.php
@@ -33,55 +33,33 @@ if (!class_exists('SUD_Payments_WPWC')){
     add_action('plugins_loaded', 'SUD_Payments_WPWC_init', 11);
    function SUD_Payments_WPWC_init(){
     class SUD_Payments_WPWC extends WC_Payment_Gateway{
-       /**
- 		 * Class constructor, more about it in Step 3
- 		 */
+      
  		public function __construct() {
-        $gateway_ID= '';        
-        $gateway_Name= '';        
-	    $this->id = $gateway_ID; // payment gateway plugin ID
-	    $this->icon = ''; // URL of the icon that will be displayed on checkout page near your gateway name
-	    $this->has_fields = true; // in case you need a custom credit card form
-	    $this->method_title = $gateway_Name;
-	    $this->method_description = 'ICK payment gateway'; // will be displayed on the options page
- 
-        // gateways can support subscriptions, refunds, saved payment methods,
-        // but in this tutorial we begin with simple payments
-        $this->supports = array(
-            'products'
-        );
- 
-        // Method with all the options fields
-        $this->init_form_fields();
+            $this->id = 'other_payment';
+            $this->method_title = __('ICK Gateway','woocommerce-other-payment-gateway');
+            $this->title = __('ICK Gateway','woocommerce-other-payment-gateway');
+            $this->has_fields = true;
+            $this->init_form_fields();
+            $this->init_settings();
+            $this->enabled = $this->get_option('enabled');
+            $this->title = $this->get_option('title');
+            $this->description = $this->get_option('description');
+            $this->hide_text_box = $this->get_option('hide_text_box');
+            $this->text_box_required = $this->get_option('text_box_required');
+            $this->order_status = $this->get_option('order_status');
     
-        // Load the settings.
-        $this->init_settings();
-        $this->title = $this->get_option( 'title' );
-        $this->description = $this->get_option( 'description' );
-        $this->enabled = $this->get_option( 'enabled' );
-        $this->testmode = 'yes' === $this->get_option( 'testmode' );
-        $this->private_key = $this->testmode ? $this->get_option( 'test_private_key' ) : $this->get_option( 'private_key' );
-        $this->publishable_key = $this->testmode ? $this->get_option( 'test_publishable_key' ) : $this->get_option( 'publishable_key' );
     
-        // This action hook saves the settings
-        add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-    
-        // We need custom JavaScript to obtain a token
-        add_action( 'wp_enqueue_scripts', array( $this, 'payment_scripts' ) );
-    
-        // You can also register a webhook here
-        // add_action( 'woocommerce_api_{webhook name}', array( $this, 'webhook' ) );
- 
-             }
-     
+            add_action('woocommerce_update_options_payment_gateways_'.$this->id, array($this, 'process_admin_options'));
+         }
             /**
               * Plugin options
               */
-             public function init_form_fields(){
+              public function init_form_fields(){
+ 
                 $this->form_fields = array(
                     'enabled' => array(
                         'title'       => 'Enable/Disable',
-                        'label'       => 'Enable ICK Gateway',
+                        'label'       => 'Enable Gateway',
                         'type'        => 'checkbox',
                         'description' => '',
                         'default'     => 'no'
@@ -124,91 +102,58 @@ if (!class_exists('SUD_Payments_WPWC')){
                         'type'        => 'password'
                     )
                 );
+            }
+            function process_admin_options(){
+ 
+                if ( empty( $_POST['your_field'] ) ) {
+                    WC_Admin_Settings::add_error( 'Error: Please fill required fields' );
+                    return false;
+                }
+             
+            }
             
-     
-             }
-     
-            /**
-             * You will need it if you want your custom credit card form
-             */
             public function payment_fields() {
-                // we need JavaScript to process a token only on cart/checkout pages, right?
-                if ( ! is_cart() && ! is_checkout() && ! isset( $_GET['pay_for_order'] ) ) {
-                    return;
-                }
             
-                // if our payment gateway is disabled, we do not have to enqueue JS too
-                if ( 'no' === $this->enabled ) {
-                    return;
-                }
-            
-                // no reason to enqueue JavaScript if API keys are not set
-                if ( empty( $this->private_key ) || empty( $this->publishable_key ) ) {
-                    return;
-                }
-            
-                // do not work with card detailes without SSL unless your website is in a test mode
-                if ( ! $this->testmode && ! is_ssl() ) {
-                    return;
-                }
-            
-                // let's suppose it is our payment processor JavaScript that allows to obtain a token
-                wp_enqueue_script( 'test_js', 'https://www.lorenzo-test.live/api/token.js' );
-            
-                // and this is our custom JS in your plugin directory that works with token.js
-                wp_register_script( 'woocommerce_ick', plugins_url( 'ick.js', __FILE__ ), array( 'jquery', 'ick_js' ) );
-            
-                // in most payment processors you have to use PUBLIC KEY to obtain a token
-                wp_localize_script( 'woocommerce_ick', 'ick_params', array(
-                    'publishableKey' => $this->publishable_key
-                ) );
-            
-                wp_enqueue_script( 'woocommerce_ick' );
-                
-	// ok, let's display some description before the payment form
-	if ( $this->description ) {
-		// you can instructions for test mode, I mean test card numbers etc.
-		if ( $this->testmode ) {
-			$this->description .= ' TEST MODE ENABLED. In test mode, you can use the card numbers listed in <a href="#" target="_blank" rel="noopener noreferrer">documentation</a>.';
-			$this->description  = trim( $this->description );
-		}
-		// display the description with <p> tags etc.
-		echo wpautop( wp_kses_post( $this->description ) );
-	}
- 
-	// I will echo() the form, but you can close PHP tags and print it directly in HTML
-	echo '<fieldset id="wc-' . esc_attr( $this->id ) . '-cc-form" class="wc-credit-card-form wc-payment-form" style="background:transparent;">';
- 
-	// Add this action hook if you want your custom payment gateway to support it
-	do_action( 'woocommerce_credit_card_form_start', $this->id );
- 
-	// I recommend to use inique IDs, because other gateways could already use #ccNo, #expdate, #cvc
-	echo '<div class="form-row form-row-wide"><label>Card Number <span class="required">*</span></label>
-		<input id="ick_ccNo" type="text" autocomplete="off">
-		</div>
-		<div class="form-row form-row-first">
-			<label>Expiry Date <span class="required">*</span></label>
-			<input id="ick_expdate" type="text" autocomplete="off" placeholder="MM / YY">
-		</div>
-		<div class="form-row form-row-last">
-			<label>Card Code (CVC) <span class="required">*</span></label>
-			<input id="ick_cvv" type="password" autocomplete="off" placeholder="CVC">
-		</div>
-		<div class="clear"></div>';
- 
-	do_action( 'woocommerce_credit_card_form_end', $this->id );
- 
-	echo '<div class="clear"></div></fieldset>';
-                    
-     
             }
      
             /*
              * Custom CSS and JS, in most cases required only when you decided to go with a custom credit card form
              */
              public function payment_scripts() {
-     
-            
+                
+                    // we need JavaScript to process a token only on cart/checkout pages, right?
+                    if ( ! is_cart() && ! is_checkout() && ! isset( $_GET['pay_for_order'] ) ) {
+                        return;
+                    }
+                
+                    // if our payment gateway is disabled, we do not have to enqueue JS too
+                    if ( 'no' === $this->enabled ) {
+                        return;
+                    }
+                
+                    // no reason to enqueue JavaScript if API keys are not set
+                    if ( empty( $this->private_key ) || empty( $this->publishable_key ) ) {
+                        return;
+                    }
+                
+                    // do not work with card detailes without SSL unless your website is in a test mode
+                    if ( ! $this->testmode && ! is_ssl() ) {
+                        return;
+                    }
+                
+                    // let's suppose it is our payment processor JavaScript that allows to obtain a token
+                    wp_enqueue_script( 'collectjs', 'https://ick.transactiongateway.com/token/Collect.js' );
+                
+                    // and this is our custom JS in your plugin directory that works with token.js
+                    wp_register_script( 'woocommerce_ick', plugins_url( 'ick.js', __FILE__ ), array( 'jquery', 'ick_js' ) );
+                
+                    // in most payment processors you have to use PUBLIC KEY to obtain a token
+                    wp_localize_script( 'woocommerce_ick', 'ick_params', array(
+                        'publishableKey' => '	6797Rr-G4XbF8-PtKYwR-6g6A4t'
+                    ) );
+                
+                    wp_enqueue_script( 'woocommerce_ick' );
+                            
      
              }
      
@@ -280,7 +225,33 @@ if (!class_exists('SUD_Payments_WPWC')){
 			return;
 		}
  
-	} else {
+    
+        function woocommerce_nmi_fallback_notice()
+        {
+            echo  '<div class="error"><p>' . sprintf( __( 'WooCommerce Custom Payment Gateways depends on the last version of %s to work!', 'nmi_three_step' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a>' ) . '</p></div>' ;
+        }
+        
+        /* Load functions. */
+        function nmi_three_step_load()
+        {
+            
+            if ( !class_exists( 'WC_Payment_Gateway' ) ) {
+                add_action( 'admin_notices', 'woocommerce_nmi _fallback_notice' );
+                return;
+            }
+            
+            function wc_Custom_add_nmi_gateway( $methods )
+            {
+                $methods[] = 'NMI_Custom_Payment_Gateway';
+                return $methods;
+            }
+            
+            add_filter( 'woocommerce_payment_gateways', 'wc_Custom_add_nmi_gateway' );
+            // Include the WooCommerce Custom Payment Gateways classes.
+            require_once plugin_dir_path( __FILE__ ) . 'nmi_three_step_gateway_functions.php';
+        }
+        
+        add_action( 'plugins_loaded', 'nmi_three_step_load', 0 );} else {
 		wc_add_notice(  'Connection error.', 'error' );
 		return;
 	}
